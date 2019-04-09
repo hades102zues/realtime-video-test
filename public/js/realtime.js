60a35ad13a4cadb111b5ba97500a3562514ea820
Joshua Wiggins <joshua_kar@hotmail.com>
2019-04-09 15:20:46 -0400

    Establishes a data channel!

--- public/js/realtime.js
+++ public/js/realtime.js
@@ -2,6 +2,7 @@ const socket = io();
 const localVideo = document.querySelector("#localVideo");
 const remoteVideo = document.querySelector("#remoteVideo");
 const callButton = document.querySelector("#call-button");
+const chatBox = document.querySelector("#chat-box");
 
 let localStream; //holds the local video feed
 let peerConnection = null; //will hold the peer connection object
@@ -34,7 +35,15 @@ window.RTCSessionDescription =
 	window.mozRTCSessionDescription ||
 	window.webkitRTCSessionDescription;
 
-//***funcs
+const addMessageToChatbox = event => {
+	console.log("Message", event);
+	const p = document.createElement("p");
+	p.appendChild(document.createTextNode(event.data));
+	chatBox.appendChild(p);
+};
+
+//***The Initiator's Side ***//
+
 const sessionDescriptionHandler = sessionDescription => {
 	//save the sessionDescription Locally then send it through
 	//the signalling server
@@ -47,16 +56,16 @@ const sessionDescriptionHandler = sessionDescription => {
 	);
 };
 
+const connectRemoteStreamToVideo = event => {
+	remoteVideo.srcObject = event.stream;
+};
+
 const forwardIceCanidate = event => {
 	if (event.candidate !== null)
 		socket.emit("outgoing", { ice: event.candidate });
 };
 
-const connectRemoteStreamToVideo = event => {
-	remoteVideo.srcObject = event.stream;
-};
-
-const createPeerConnection = () => {
+const createPeerConnection = createDataChannel => {
 	//the stun server tells us our public ip that is otherwise hidden behind NAT
 	peerConnection = new RTCPeerConnection({
 		iceServers: [
@@ -68,10 +77,8 @@ const createPeerConnection = () => {
 	//connect up our own stream to the peer connection
 	peerConnection.addStream(localStream);
 
-	/*
-	 		next we specify what should happen when certain
-	 						events take place
-	*/
+	/*	next we specify what should happen when certain
+	 		events take place */
 
 	/*
 	upon receiving our details from the stun server, send them to the other client
@@ -84,13 +91,31 @@ const createPeerConnection = () => {
 		attach it to remote video tag
 	 */
 	peerConnection.onaddstream = connectRemoteStreamToVideo;
+
+	//if we donot want to create a data channel
+	if (!createDataChannel) return;
+
+	/****** open up a data channel******/
+	//initiate a channel
+	const dataChannel = peerConnection.createDataChannel("Tunnel");
+
+	//once the connection on both ends of the channel is established
+	dataChannel.onopen = () => {
+		dataChannel.send("Hi, I invited you to this call!");
+	};
+
+	dataChannel.onclose = () => console.log("The Data Channel is Closed");
+
+	dataChannel.onerror = () => console.log("Error on data Channel");
+
+	dataChannel.onmessage = addMessageToChatbox;
 };
 
 const callButtonHandler = () => {
-	//create the peer connection object
-	createPeerConnection();
+	//go ahead and create the peer connection object
+	createPeerConnection(true);
 
-	//send an offer
+	//send a call offer
 	peerConnection.createOffer(sessionDescriptionHandler, err => {
 		console.log("Error on Offer Generation");
 	});
@@ -98,7 +123,6 @@ const callButtonHandler = () => {
 
 const onLocalStream = stream => {
 	localStream = stream;
-
 	//append stream to local video
 	localVideo.srcObject = stream;
 };
@@ -118,10 +142,14 @@ const onPageStart = () => {
 onPageStart();
 callButton.addEventListener("click", callButtonHandler);
 
-//Receiver
+//***The Following is everthing that happens on the Receiver's end***//
+
+//server relays a message from the Initiator
 socket.on("incoming", msg => {
 	//First create a peer connection object to establish a connection, if none exists
-	if (peerConnection === null) createPeerConnection();
+	//we pass false her because then we'd be creating our on data channel,
+	//given how i set up the code
+	if (peerConnection === null) createPeerConnection(false);
 
 	//Now the incoming message can be either
 	// 		iceCanidate OR sessionDescription
@@ -146,4 +174,17 @@ socket.on("incoming", msg => {
 			}
 		);
 	}
+
+	//now we handle any data channels requests
+	peerConnection.ondatachannel = event => {
+		event.channel.onopen = () => {
+			event.channel.send("Thanks for inviting me caller!");
+		};
+
+		event.channel.onclose = () => console.log("The Data Channel is Closed");
+
+		event.channel.onerror = () => console.log("Error on data Channel");
+
+		event.channel.onmessage = addMessageToChatbox;
+	};
 });
